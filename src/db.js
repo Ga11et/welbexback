669bc32362d65e569d5a920b89a5ab3b739ec36e
Fyodor <sivkov1998@yandex.ru>
2022-09-25 02:50:54 +0300

    first one

--- src/db.js
+++ src/db.js
@@ -2,10 +2,6 @@ const Pool = require('pg-pool')
 const dotenv = require('dotenv').config()
 
 module.exports = new Pool({
-  database: process.env.DB_BASENAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  port: 5432,
-  host: process.env.DB_HOST,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 })
